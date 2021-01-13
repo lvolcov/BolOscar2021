35150436835a23734f68dd8809ae3623db25801a
Lucas Volcov <lucas.volcov@hotmail.com>
2021-01-13 12:21:32 +0000

    segundo dia de trabalho

--- app.js
+++ app.js
@@ -2,102 +2,68 @@
 require ('custom-env').env()
 const { Telegraf } = require('telegraf')
 const db = require('./db.json');
-
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+bot.start((ctx) => {ctx.reply("comecouuu \n - /menu")})
 
-
-bot.start((ctx) => {
-    ctx.reply("comecouuu \n - /help")
-})
-
-bot.help((ctx) => {
-    ctx.reply("escolhe entre \n -/start \n -/help \n -/menu")
-})
+bot.help((ctx) => {ctx.reply("escolhe entre \n -/start \n -/help \n -/menu")})
 
 bot.command("menu", (ctx) => {
-    ctx.telegram.sendMessage(ctx.chat.id, "Menu: ", 
-    {
-        reply_markup: {
-            inline_keyboard: [
-                [{text: "Filme", callback_data: "selected"},{text: "Diretor", callback_data: "diretor"}],
-                [{text: "Dir. Fotografia", callback_data: "selected"},{text: "Dir. Arte", callback_data: "selected"}],
-                [{text: "Roteiro Adaptado", callback_data: "selected"},{text: "Roteiro Original", callback_data: "selected"}],
-                [{text: "Atriz Principal", callback_data: "selected"},{text: "Ator Principal", callback_data: "selected"}],
-                [{text: "Atriz Coadjuvante", callback_data: "selected"},{text: "Ator Coadjuvante", callback_data: "selected"}],
-                [{text: "Efeitos Visuais", callback_data: "selected"},{text: "Edição", callback_data: "selected"}],
-                [{text: "Animação", callback_data: "selected"},{text: "Estrangeiro", callback_data: "selected"}],
-                [{text: "Mixagem de Som", callback_data: "selected"},{text: "Edição de Som", callback_data: "selected"}],
-                [{text: "Figurino", callback_data: "selected"},{text: "Maquiagem", callback_data: "selected"}],
-                [{text: "Documentário", callback_data: "selected"},{text: "Trilha Sonora", callback_data: "selected"}],
-                [{text: "Curta-Metragem", callback_data: "selected"},{text: "Canção Original", callback_data: "selected"}],
-                [{text: "Anim. Curta-Metragem", callback_data: "selected"},{text: "Doc. Curta-Metragem", callback_data: "selected"}],
-            ]
-        }
+    ctx.deleteMessage()
+    const makeResult = Object.keys(db.categorias).map((elem, index) =>{
+        return {text : db.categorias[elem].nomeMenu, callback_data: db.categorias[elem].nomeResumido}
+    })
+    const result = geraLista(makeResult)
+    ctx.telegram.sendMessage(ctx.chat.id, "Menu:", 
+    {reply_markup: {inline_keyboard: result}
     })
 })
 
 
 bot.action("menu", (ctx) => {
     ctx.deleteMessage()
-    ctx.telegram.sendMessage(ctx.chat.id, "Menu: ", 
-    {
-        reply_markup: {
-            inline_keyboard: [
-                [{text: "Filme", callback_data: "selected"},{text: "Diretor", callback_data: "diretor"}],
-                [{text: "Dir. Fotografia", callback_data: "selected"},{text: "Dir. Arte", callback_data: "selected"}],
-                [{text: "Roteiro Adaptado", callback_data: "selected"},{text: "Roteiro Original", callback_data: "selected"}],
-                [{text: "Atriz Principal", callback_data: "selected"},{text: "Ator Principal", callback_data: "selected"}],
-                [{text: "Atriz Coadjuvante", callback_data: "selected"},{text: "Ator Coadjuvante", callback_data: "selected"}],
-                [{text: "Efeitos Visuais", callback_data: "selected"},{text: "Edição", callback_data: "selected"}],
-                [{text: "Animação", callback_data: "selected"},{text: "Estrangeiro", callback_data: "selected"}],
-                [{text: "Mixagem de Som", callback_data: "selected"},{text: "Edição de Som", callback_data: "selected"}],
-                [{text: "Figurino", callback_data: "selected"},{text: "Maquiagem", callback_data: "selected"}],
-                [{text: "Documentário", callback_data: "selected"},{text: "Trilha Sonora", callback_data: "selected"}],
-                [{text: "Curta-Metragem", callback_data: "selected"},{text: "Canção Original", callback_data: "selected"}],
-                [{text: "Anim. Curta-Metragem", callback_data: "selected"},{text: "Doc. Curta-Metragem", callback_data: "selected"}],
-            ]
-        }
+    const makeResult = Object.keys(db.categorias).map((elem) =>{
+        return {text : db.categorias[elem].nomeMenu, callback_data: db.categorias[elem].nomeResumido}
     })
+    const result = geraLista(makeResult)
+    ctx.telegram.sendMessage(ctx.chat.id, "Menu:", {reply_markup: {inline_keyboard: result}})
 })
 
 
 bot.on('callback_query', (ctx) => {
+    const called = ctx.update.callback_query.data
+    const typeCalled = called.substring(0,3)
+    // answerCallbackQuery(called, text = 'voto contabilizado', show_alert = true, url = NULL, cache_time = NULL)
     ctx.deleteMessage()
-    console.log(ctx.update.callback_query.from)
-    ctx.telegram.sendMessage(ctx.chat.id, "Melhor Diretor: ", 
-    {
-        reply_markup: {
-            inline_keyboard: [
-                [{text: "Martin Scorsese - O Irlandês", callback_data: "menu"},{text: "Todd Phillips - Coringa", callback_data: "menu"}],
-                [{text: "Sam Mendes - 1917", callback_data: "menu"},{text: "Bong Joon Ho - Parasita", callback_data: "menu"}],
-                [{text: "Quentin Tarantino - Era uma vez em... Hollywood", callback_data: "menu"}],
-            ]
-        }
+    if (typeCalled === 'cat') {
+        const makeResult = Object.keys(db.categorias[called].indicados).map((elem) =>{
+            return {text : db.categorias[called].indicados[elem].nomeCompleto, callback_data: db.categorias[called].indicados[elem].nomeResumido}
+        })
+        const result = geraLista(makeResult)
+        ctx.telegram.sendMessage(ctx.chat.id, 'Categoria ' + db.categorias[called].nomeCompleto + ':', {reply_markup: {inline_keyboard: result}})
+    }
+    else{
+        ///
+        /// INCLUIR CONTAGEM DOS VOTOS
+        ///
+        const makeResult = Object.keys(db.categorias).map((elem) =>{
+            return {text : db.categorias[elem].nomeMenu, callback_data: db.categorias[elem].nomeResumido}
+        })
+        const result = geraLista(makeResult)
+        ctx.telegram.sendMessage(ctx.chat.id, "Menu:", {reply_markup: {inline_keyboard: result}})
+    }
     })
-})
 
-
-// bot.action("diretor", (ctx) => {
-//     ctx.deleteMessage()
-//     console.log(ctx)
-//     ctx.telegram.sendMessage(ctx.chat.id, "Melhor Diretor: ", 
-//     {
-//         reply_markup: {
-//             inline_keyboard: [
-//                 [{text: "Martin Scorsese - O Irlandês", callback_data: "menu"},{text: "Todd Phillips - Coringa", callback_data: "menu"}],
-//                 [{text: "Sam Mendes - 1917", callback_data: "menu"},{text: "Bong Joon Ho - Parasita", callback_data: "menu"}],
-//                 [{text: "Quentin Tarantino - Era uma vez em... Hollywood", callback_data: "menu"}],
-//             ]
-//         }
-//     })
-// })
-
-
-bot.use((ctx) => {
-    ctx.reply("voltar \n -/start")
-
-})
-
+bot.use((ctx) => {ctx.reply("voltar \n -/start")})
 
 bot.launch()
+
+function geraLista (items) {
+    const result = []
+    let i = 0
+    while (i < items.length){
+        items[i+1] === undefined ? result.push([items[i]]) : result.push([items[i], items[i+1]])
+        i += 2
+    }
+    return result
+}
